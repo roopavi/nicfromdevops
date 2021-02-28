7974e782f84b103a531c7576c8bd2b8dd209c4bd
Roopa <roopa.grsv@gmail.com>
2021-02-28 15:27:55 +0530

    nicfromdevops

--- main input.tf
+++ main input.tf
@@ -1,24 +1,24 @@
 resource "azurerm_resource_group" "example" {
-  name     = "NICRG1"
+  name     = "NICRG2"
   location = "West Europe"
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "nicvnetwork1"
+  name                = "nicvnetwork2"
   address_space       = ["192.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "example" {
-  name                 = "devsub1"
+  name                 = "devsub2"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["192.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "example" {
-  name                = "NIC1"
+  name                = "NIC2"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
